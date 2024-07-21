1db22c754ea869e5894278703a7ce79251877fd7
Adam SOIMANSOIB <adamsoimansoibpro@gmail.com>
2024-07-21 20:14:38 +0200

    [feat] initial commit

---
+++ src/main/java/dev/astranfalio/activityweather/ActivityWeatherApplication.java
@@ -0,0 +1,13 @@
+package dev.astranfalio.activityweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityWeatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/astranfalio/activityweather/ActivityWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.astranfalio.activityweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
